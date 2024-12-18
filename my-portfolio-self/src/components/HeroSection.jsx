58466cdac3387165fc77e296732af41205110da0
Divy <divparikh0511@gmail.com>
2024-12-17 19:35:26 -0500

    final
    final commit

--- my-portfolio-self/src/components/HeroSection.jsx
+++ my-portfolio-self/src/components/HeroSection.jsx
@@ -6,9 +6,8 @@ const HeroSection = () => {
     <section className="hero-section">
       <div className="hero-content">
         <h1>I'm Divy.</h1>
-        <h2>I Code * Create * Innovate.</h2>
-        <p>Empowering Ideas Through End-to-End Development</p>
-        <p>Expertise and Seamless User Interfaces</p>
+        <h2>I love to innovate and analyze.</h2>
+        <p>I am a Computer Science student at York University with a passion for software development and data analytics. My goal is to leverage my skills in programming and data-driven decision-making to create innovative solutions and drive impactful results</p>
       </div>
       <div className="hero-image">
         {/* Replace with your actual image */}
